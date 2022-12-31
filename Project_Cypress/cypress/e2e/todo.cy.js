f5516131586b8896c8325d69626b606d944b84be
yasminvasconceloss <yasminvasconcelos59@gmail.com>
2022-12-31 19:00:14 -0300

    inserir texto no campo/adicionar à lista
    +Asserção: Tamanho correto da lista.

--- Project_Cypress/cypress/e2e/todo.cy.js
+++ Project_Cypress/cypress/e2e/todo.cy.js
@@ -4,6 +4,15 @@ context('TodoMVC', () => {
         cy.visit('http://todomvc-app-for-testing.surge.sh/')
         
 
-       //cy.get('input.new-todo')
+       cy.get('input.new-todo')
+        .type('Dormir')
+        .type('{enter}')
+
+    // asserção: validação/garantia 
+        //cy.get('ul.todo-list li').should('have.lenght', 3)
+
     });
+    
 });
+
+//chai
