8bd1ca228a6f9311b3dae380b4ca559ae4fe04b0
yasminvasconceloss <yasminvasconcelos59@gmail.com>
2022-12-30 23:20:18 -0300

    Arquivos Cypress
    cy.visit

---
+++ Project_Cypress/cypress/e2e/todo.cy.js
@@ -0,0 +1,9 @@
+
+context('TodoMVC', () => {
+    it('Adicionar nova tarefa', () => {
+        cy.visit('http://todomvc-app-for-testing.surge.sh/')
+        
+
+       //cy.get('input.new-todo')
+    });
+});
